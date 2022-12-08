089aa7defa62d90e5897bfac87c56e5004f07523
yusrilekamahendra <projectyusril@gmail.com>
2022-12-08 09:48:14 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/donut/yusrilekamahendra/donut_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.donut.yusrilekamahendra.donut_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,131 @@
+import 'package:donut_app/tab/burger_tab.dart';
+import 'package:donut_app/tab/donut_tab.dart';
+import 'package:donut_app/tab/pancake_tab.dart';
+import 'package:donut_app/tab/pizza_tab.dart';
+import 'package:donut_app/tab/smoothie_tab.dart';
+import 'package:donut_app/util/my_tab.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // my tabs
+  List<Widget> myTabs = const [
+    // donut tab
+    MyTab(
+      iconPath: 'assets/icons/donut.png',
+    ),
+
+    // burger tab
+    MyTab(
+      iconPath: 'assets/icons/burger.png',
+    ),
+
+    // smoothie tab
+    MyTab(
+      iconPath: 'assets/icons/smoothie.png',
+    ),
+
+    // pancake tab
+    MyTab(
+      iconPath: 'assets/icons/pancakes.png',
+    ),
+
+    // pizza tab
+    MyTab(
+      iconPath: 'assets/icons/pizza.png',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 24.0),
+            child: IconButton(
+              icon: Icon(
+                Icons.menu,
+                color: Colors.grey[800],
+                size: 36,
+              ),
+              onPressed: () {
+                // open drawer
+              },
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
+              child: IconButton(
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.grey[800],
+                  size: 36,
+                ),
+                onPressed: () {
+                  // account button tapped
+                },
+              ),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            // i want to eat
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 36.0, vertical: 18),
+              child: Row(
+                children: [
+                  Text(
+                    'I want to eat',
+                    style: TextStyle(fontSize: 24, color: Colors.grey[600]),
+                  ),
+                  Text(
+                    ' EAT',
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+
+            // tab bar
+            TabBar(tabs: myTabs),
+
+            // tab bar view
+            Expanded(
+              child: TabBarView(
+                children: [
+                  // donut page
+                  DonutTab(),
+
+                  // burger page
+                  BurgerTab(),
+
+                  // smoothie page
+                  SmoothieTab(),
+
+                  // pancake page
+                  PancakeTab(),
+
+                  // pizza page
+                  PizzaTab(),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
